119c93fce049e15b9281d2f5837dee8c9f890986
Emilien Mantel <emilien.mantel@gmail.com>
2018-03-18 19:18:55 +0100

    Fix ubuntu builds

--- ubuntu18.04/run.sh
+++ ubuntu18.04/run.sh
@@ -2,6 +2,34 @@
 
 set -e
 
+cat > "/usr/sbin/install_packages" <<-'EOF'
+#!/bin/sh
+set -e
+set -u
+export DEBIAN_FRONTEND=noninteractive
+n=0
+max=2
+until [ $n -gt $max ]; do
+    set +e
+    (
+      apt-get update -qq &&
+      apt-get install -y --no-install-recommends "$@"
+    )
+    CODE=$?
+    set -e
+    if [ $CODE -eq 0 ]; then
+        break
+    fi
+    if [ $n -eq $max ]; then
+        exit $CODE
+    fi
+    echo "apt failed, retrying"
+    n=$(($n + 1))
+done
+rm -r /var/lib/apt/lists /var/cache/apt/archives
+EOF
+chmod 0755 "/usr/sbin/install_packages"
+
 install_packages sudo openssh-server python wget ca-certificates
 mkdir -p /var/run/sshd && chmod 0755 /var/run/sshd
 echo 'UseDNS no' >> /etc/ssh/sshd_config
